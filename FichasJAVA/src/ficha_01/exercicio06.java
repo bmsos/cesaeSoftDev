c0aaca65cf19bf42afc781db10c882375d367529
bmsos <bmsos@outlook.pt>
2024-02-19 13:00:50 +0000

    first_exercises

---
+++ FichasJAVA/src/ficha_01/exercicio06.java
@@ -0,0 +1,15 @@
+package ficha_01;
+import java.util.Scanner;
+import java.util.Scanner;
+
+public class exercicio06 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int v1, v2;
+        System.out.print("Indique o primeiro valor: ");
+        v1 = input.nextInt();
+        System.out.print("Indique o segundo valor: ");
+        v2 = input.nextInt();
+
+    }
+}
