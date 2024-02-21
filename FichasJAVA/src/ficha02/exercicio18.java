6caf09b870c180e5c371242e433130e6355c4c2e
bmsos <bmsos@outlook.pt>
2024-02-21 13:04:29 +0000

    new_exercises

--- FichasJAVA/src/ficha02/exercicio18.java
+++ FichasJAVA/src/ficha02/exercicio18.java
@@ -1,36 +1,41 @@
-package ficha02;
-import java.util.Scanner;
-public class exercicio18 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-        double vencimentoBase = 40, subsidioAlimentacao = 5, retencaoIRS = 0.1,
-                encargoFuncionario = 0.11, encargoEmpresa = 0.2375,
-                valorLiquidoVencimento, totalSubsidioAlimentacao,
-                valorEntregaIRS, totalSegSocial, totalLiquido, vencimentoBruto;
-
-        int codigoFunc, nrDias;
-
-
-        System.out.print("Indique o código do funcionário: ");
-        codigoFunc = input.nextInt();
-        System.out.print("Quantos dias trabalhou este mês?: ");
-        nrDias = input.nextInt();
-
-        totalSubsidioAlimentacao = subsidioAlimentacao * nrDias;
-        vencimentoBruto = vencimentoBase * nrDias;
-        valorEntregaIRS = vencimentoBruto * retencaoIRS;
-        valorLiquidoVencimento = vencimentoBruto * (1- (retencaoIRS + encargoFuncionario));
-        totalSegSocial = vencimentoBruto * (encargoEmpresa + encargoFuncionario);
-        totalLiquido = valorLiquidoVencimento + totalSubsidioAlimentacao;
-
-        System.out.println("Código do Funcionário: " + codigoFunc);
-        System.out.println("Valor líquido fruto do vencimento: " + valorLiquidoVencimento);
-        System.out.println("Total subsídio alimentação: " + totalSubsidioAlimentacao);
-        System.out.println("Retenção IRS a entregar ao Estado: " + valorEntregaIRS);
-        System.out.println("Valor a entregar à Segurança Social: " + totalSegSocial);
-        System.out.println("Valor líquido a receber pelo funcionário: " + totalLiquido);
-
-
-    }
-}
+//package ficha02;
+//import java.util.Scanner;
+//public class exercicio18 {
+//    public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+//
+//        double vencimentoBase, subsidioAlimentacao, retencaoIRS = 0.1,
+//                encargoFuncionario = 0.11, encargoEmpresa = 0.2375,
+//                valorLiquidoVencimento, totalSubsidioAlimentacao,
+//                valorEntregaIRS, totalSegSocial, totalLiquido, vencimentoBruto;
+//
+//        int nrDias, codigoFuncionario;
+//
+//        String cargo;
+//
+//        System.out.print("Indique o seu código de funcionário): ");
+//        codigoFuncionario = input.nextInt();
+//        input.nextLine();
+//        System.out.print("Indique o seu cargo (E - empregado | C - chefe | A - administrador): ");
+//        cargo = input.nextLine();
+//        System.out.print("Quantos dias trabalhou este mês?: ");
+//        nrDias = input.nextInt();
+//
+//        totalSubsidioAlimentacao = subsidioAlimentacao * nrDias;
+//        vencimentoBruto = vencimentoBase * nrDias;
+//        valorEntregaIRS = vencimentoBruto * retencaoIRS;
+//        valorLiquidoVencimento = vencimentoBruto * (1- (retencaoIRS + encargoFuncionario));
+//        totalSegSocial = vencimentoBruto * (encargoEmpresa + encargoFuncionario);
+//        totalLiquido = valorLiquidoVencimento + totalSubsidioAlimentacao;
+//
+//        System.out.println("Código de funcionário: " + codigoFuncionario);
+//        System.out.println("Cargo: " + cargo);
+//        System.out.println("Valor líquido fruto do vencimento: " + valorLiquidoVencimento);
+//        System.out.println("Total subsídio alimentação: " + totalSubsidioAlimentacao);
+//        System.out.println("Retenção IRS a entregar ao Estado: " + valorEntregaIRS);
+//        System.out.println("Valor a entregar à Segurança Social: " + totalSegSocial);
+//        System.out.println("Valor líquido a receber pelo funcionário: " + totalLiquido);
+//
+//
+//    }
+//}
