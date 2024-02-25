bb9263feeb2a0c23f346a62a2d18598f248d846d
bs-python-aveiro <132390082+bs-python-aveiro@users.noreply.github.com>
2024-02-25 22:11:35 +0000

    ficha04_completa

--- FichasJAVA/src/ficha04/exercicio04.java
+++ FichasJAVA/src/ficha04/exercicio04.java
@@ -1,4 +1,27 @@
 package ficha04;
-
+import java.util.Scanner;
 public class exercicio04 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n;
+        boolean primo = true;
+        System.out.print("Indique um número: ");
+        n = input.nextInt();
+
+        if (n != 1 && n != 2) {
+            for (int i = 2; i < n; i++) {
+                if (n % i == 0) {
+                    primo = false;
+                    break;
+                }
+            }
+        }
+
+        if (primo) {
+            System.out.println(n + " é um número primo.");
+        } else {
+            System.out.println(n + " não é um número primo.");
+        }
+    }
 }
