e5f322694741836e6d98dd761d152b4464439f39
bmsos <153396444+bmsos@users.noreply.github.com>
2024-03-01 16:50:50 +0000

    web_01032024

--- FichasJAVA/src/extra_ciclos/exercicio09.java
+++ FichasJAVA/src/extra_ciclos/exercicio09.java
@@ -1,4 +1,16 @@
 package extra_ciclos;
-
+import java.util.Scanner;
 public class exercicio09 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n;
+
+        System.out.print("Numero: ");
+        n = input .nextInt();
+
+        int reps = (n / 10) + 1;
+
+        System.out.println(n % 10);
+    }
 }
