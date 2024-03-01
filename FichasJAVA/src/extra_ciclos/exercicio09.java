d2242add16690eabe5b664bb1fba438959527a57
bs-python-aveiro <132390082+bs-python-aveiro@users.noreply.github.com>
2024-03-01 22:22:17 +0000

    completed_fichasJava

--- FichasJAVA/src/extra_ciclos/exercicio09.java
+++ FichasJAVA/src/extra_ciclos/exercicio09.java
@@ -4,13 +4,16 @@ public class exercicio09 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int n;
+        int n, resto, res= 0;;
 
         System.out.print("Numero: ");
         n = input .nextInt();
 
-        int reps = (n / 10) + 1;
-
-        System.out.println(n % 10);
+        while (n != 0) {
+            resto = n % 10;
+            res += resto;
+            n /= 10;
+        }
+        System.out.print("Soma dos algarismos: " + res);
     }
 }
--- FichasJAVA/src/extra_ciclos/exercicio10.java
+++ FichasJAVA/src/extra_ciclos/exercicio10.java
@@ -1,4 +1,17 @@
 package extra_ciclos;
-
+import java.util.Scanner;
 public class exercicio10 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n,resto, res=0;
+        System.out.print("Número: ");
+        n = input.nextInt();
+
+        while (n != 0) {
+            resto = n % 10;
+            res += resto;
+            n /= 10;
+            System.out.print(resto);
+        }
+    }
 }
