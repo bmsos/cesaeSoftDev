f7df656d515bb138f270cb762e3125cd4cd17695
bmsos <153396444+bmsos@users.noreply.github.com>
2024-03-11 23:42:03 +0000

    added exceptions

---
+++ programacaoVitor/PCE/exerciciosResolvidos/fichasJava/src/exceptions/teste.java
@@ -0,0 +1,11 @@
+package exceptions;
+
+public class teste {
+    public static void main(String[] args) {
+        try {
+            int a = "20/";
+        } catch (Error e) {
+            System.out.print(e);
+        }
+    }
+}
