7ded761fc0ae1f09cc33f715a1cb211407b74e92
bmsos <153396444+bmsos@users.noreply.github.com>
2024-03-18 18:55:11 +0000

    continuação ex3 extra_ficheiros pce

--- programacao_vitor/PCE/exerciciosResolvidos/fichasJava/src/extra_ficheiros/ex3.java
+++ programacao_vitor/PCE/exerciciosResolvidos/fichasJava/src/extra_ficheiros/ex3.java
@@ -1,9 +1,6 @@
 package extra_ficheiros;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
 public class ex3 {
@@ -44,7 +41,16 @@ public class ex3 {
 
         return false;
     }
-    static String autoIncrementarId (File file) throws FileNotFoundException {
+    static int contarLinhas (File file) throws FileNotFoundException {
+        Scanner sc = new Scanner(file);
+        int linhas = 0;
+        while (sc.hasNextLine()) {
+            sc.nextLine();
+            linhas++;
+        }
+        return linhas;
+    }
+    static String autoIncrementarId (File file, String docIdentifier) throws FileNotFoundException {
         Scanner sc = new Scanner(file);
         // numero vai ser auto incrementado (atraves do numero do ultimo registo na lista)
         String lastId = "000000";
@@ -55,22 +61,34 @@ public class ex3 {
         lastId = lastId.split("-")[1];
         // "0042".split("0") --> {"", "", "42"}
         String[] purificadorNumero = lastId.split("0");      // criar um array para depois poder acrescentar uma quantidade de zeros à esquerda baseada em length da lista -1
-        String newId = "c-";
+        String newId = docIdentifier + "-";
         for (int i = 0; i< purificadorNumero.length-1; i++) {
             newId += "0";
         }
         newId += Integer.toString(Integer.parseInt(purificadorNumero[purificadorNumero.length-1]) + 1);
         return newId;
     }
+    static boolean verificarNumQuartoJaExiste (String num) throws FileNotFoundException {
+        File fileQuartosHotel = new File("filesExtra/Ex_03 Hotel Temático/quartosHotel.csv");
+        Scanner sc = new Scanner(fileQuartosHotel);
+        while (sc.hasNextLine()) {
+            String numQuarto = sc.nextLine().split(";")[0];
+            if (numQuarto.equals(num)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
     // 1. gerir bases de dados
-    // 1.1. Gerir clientes
-    // 1.1.1. Adicionar novo cliente
-    static void adicionarNovoCliente (File file) throws IOException {
+        // 1.1. Gerir clientes
+            // 1.1.1. Adicionar novo cliente
+    static void adicionarNovoCliente () throws IOException {
+        File fileClientesHotel = new File("filesExtra/Ex_03 Hotel Temático/clientesHotel.csv");
         Scanner input = new Scanner(System.in);
-        FileWriter fw = new FileWriter(file, true);
+        FileWriter fw = new FileWriter(fileClientesHotel, true);
         String cliente = "";
-        cliente += (autoIncrementarId(file) + ";");
+        cliente += (autoIncrementarId(fileClientesHotel, "c") + ";");
         System.out.print("Nome: ");
         cliente += (input.nextLine() + ";");
         System.out.print("Data de nascimento (DD-MM-YYYY): ");
@@ -83,19 +101,97 @@ public class ex3 {
         fw.close();
         System.out.println("Cliente adicionado\n");
     }
+            // 1.1.2. AtualizarContactosCliente
+    static void atualizarContactoCliente (String id) throws FileNotFoundException {
+        Scanner input = new Scanner(System.in);
+        String tipoContacto = "", novoValor="";
 
-    public static void main(String[] args) throws IOException {
+        System.out.print("""
+           Atualizar contacto:
+           1. Email
+           2. Telefone
+           """);
+        int opcaoAtualizarContacto= 0;
+        while (opcaoAtualizarContacto< 1 || opcaoAtualizarContacto> 2) {
+            System.out.print("Opção (1-2): ");
+            opcaoAtualizarContacto= input.nextInt();
+
+            switch (opcaoAtualizarContacto) {
+                case 1 -> {
+                    System.out.print("Novo Email: ");
+                    input.nextLine();
+                    novoValor = input.nextLine();
+                    tipoContacto = "email";
+                }
+                case 2 -> {
+                    System.out.print("Novo contacto telefónico: ");
+                    input.nextLine();
+                    novoValor = input.nextLine();
+                    tipoContacto = "telefone";
+                }
+                default -> System.out.println("Opção inválida.");
+            }
+        }
+
         File fileClientesHotel = new File("filesExtra/Ex_03 Hotel Temático/clientesHotel.csv");
+        Scanner sc = new Scanner(fileClientesHotel);
+        String[] dados = new String[contarLinhas(fileClientesHotel)];
+
+        int index = 0;
+        while (sc.hasNextLine()) {
+            String cliente = sc.nextLine(),
+            idCliente = cliente.split(";")[0];
+            if (!idCliente.equals(id)) {
+                dados[index] = cliente;
+            } else {
+                String clienteAtualizado = cliente.split(";")[0] + ";" + cliente.split(";")[1] + ";" + cliente.split(";")[2] + ";";
+                if (tipoContacto.equals("telefone")) {
+                    clienteAtualizado += (novoValor + ";" + cliente.split(";")[4]);
+                } else {    // tipoContacto.equals("email")
+                    clienteAtualizado += (cliente.split(";")[3] + ";" + novoValor + ";");
+                }
+                dados[index] = clienteAtualizado;
+            }
+            index++;
+        }
+
+        PrintWriter pw = new PrintWriter(fileClientesHotel);
+        for (String e : dados) {
+            pw.println(e);
+        }
+        pw.close();
+
+    }
+        // 1.2. Adicionar Tema
+    static void adicionarTema (String nome, String multiplicadorPreco) throws IOException {
+        File fileTemasHotel = new File("filesExtra/Ex_03 Hotel Temático/temasHotel.csv");
+        String id = autoIncrementarId(fileTemasHotel, "t"),
+        novoTema = id + ";" + nome + ";" + multiplicadorPreco;
+
+        FileWriter fw = new FileWriter(fileTemasHotel, true);
+        fw.write(novoTema + "\n");
+        fw.close();
+    }
+        // 1.3. Adicionar quarto
+    static void adicionarQuarto (String numQuarto, String tema, String tipologia) throws IOException {
+        File fileTemasHotel = new File("filesExtra/Ex_03 Hotel Temático/quartosHotel.csv");
+
+        String novoQuarto = numQuarto + ";" + tema + ";" + tipologia;
+
+        FileWriter fw = new FileWriter(fileTemasHotel, true);
+        fw.write(novoQuarto + "\n");
+        fw.close();
+        }
+
+    public static void main(String[] args) {
         File fileProdutosHotel = new File("filesExtra/Ex_03 Hotel Temático/produtosHotel.csv");
-        File fileQuartosHotel = new File("filesExtra/Ex_03 Hotel Temático/quartosHotel.csv");
         File fileReservasHotel = new File("filesExtra/Ex_03 Hotel Temático/reservasHotel.csv");
         File fileServicoQuartoHotel = new File("filesExtra/Ex_03 Hotel Temático/servicoQuartoHotel.csv");
-        File fileTemasHotel = new File("filesExtra/Ex_03 Hotel Temático/temasHotel.csv");
         File fileGaleriaFachadaHotel = new File("filesExtra/Ex_03 Hotel Temático/galeriaFachadaHotel.txt");
         File fileGaleriaQuartoHotel = new File("filesExtra/Ex_03 Hotel Temático/galeriaQuartoHotel.txt");
         File fileGaleriaSpaHotel = new File("filesExtra/Ex_03 Hotel Temático/galeriaSpaHotel.txt");
 
-//        try {
+        try {
             boolean sessaoIniciada = login();
             while (sessaoIniciada) {
                 Scanner input = new Scanner(System.in);
@@ -112,14 +208,14 @@ public class ex3 {
                 """);
                 int opcao = 0;
                 while (opcao < 1 || opcao > 7) {
-                    System.out.println("Opção (1-7):");
+                    System.out.print("Opção (1-7): ");
                     opcao = input.nextInt();
 
                     switch (opcao) {
 // 1. Gerir Base de Dados
                         case 1 -> {
                             System.out.print("""
-                            Gerir base de dados:
+                            \nGerir base de dados:
                             1. Clientes
                             2. Adicionar tema
                             3. Adicionar quarto
@@ -128,44 +224,83 @@ public class ex3 {
                             """);
                             int opcaoGerirBaseDados = 0;
                             while (opcaoGerirBaseDados < 1 || opcaoGerirBaseDados > 5) {
-                                System.out.println("Opção (1-5):");
+                                System.out.print("Opção (1-5): ");
                                 opcaoGerirBaseDados = input.nextInt();
 
                                 switch (opcaoGerirBaseDados) {
     // 1.1. Gerir clientes
                                     case 1 -> {
                                         System.out.print("""
-                                        Gerir clientes:
+                                        \nGerir clientes:
                                         1. Adicionar cliente
                                         2. Atualizar contactos do cliente
                                         """);
                                         int opcaoGerirClientes = 0;
                                         while (opcaoGerirClientes < 1 || opcaoGerirClientes > 2) {
-                                            System.out.println("Opção (1-2):");
+                                            System.out.print("Opção (1-2): ");
                                             opcaoGerirClientes = input.nextInt();
 
                                             switch (opcaoGerirClientes) {
         // 1.1.1. Adicionar cliente
                                                 case 1 -> {
-                                                    adicionarNovoCliente(fileClientesHotel);
+                                                    adicionarNovoCliente();
+                                                    System.out.println("Cliente adicionado.\n");
                                                 }
-        // 1.1.1. Atualizar contactos cliente
-                                                case 2 -> {}
+        // 1.1.2. Atualizar contactos cliente
+                                                case 2 -> {
+                                                    System.out.print("Id do cliente: ");
+                                                    input.nextLine();
+                                                    String id = input.nextLine();
+
+                                                    atualizarContactoCliente(id);
+                                                    System.out.println("Atualização de dados efetuada\n");
+                                                }
                                                 default -> System.out.println("Opção inválida.");
                                             }
                                         }
                                     }
-                                    case 2 -> {}
-                                    case 3 -> {}
+    // 1.2. Adicionar tema
+                                    case 2 -> {
+                                        System.out.print("Nome: ");
+                                        input.nextLine();
+                                        String nome = input.nextLine();
+                                        System.out.print("Multiplicador de preço: ");
+                                        String multiplicadorPreco = input.nextLine();
+
+                                        adicionarTema(nome, multiplicadorPreco);
+                                        System.out.println("Tema adicionado com sucesso.\n");
+                                    }
+    // 1.3. Adicionar quarto
+                                    case 3 -> {
+                                        String numQuarto;
+                                        while (true) {
+                                            System.out.print("Número do quarto: ");
+                                            input.nextLine();
+                                            numQuarto = input.nextLine();
+                                            if (verificarNumQuartoJaExiste(numQuarto)) {
+                                                System.out.println("Número já existe.");
+                                            } else {
+                                                break;
+                                            }
+                                        }
+
+                                        System.out.print("Tema: ");
+                                        String tema = input.nextLine();
+                                        System.out.print("Tipologia: ");
+                                        String tipologia = input.nextLine().toUpperCase();
+
+                                        adicionarQuarto(numQuarto, tema, tipologia);
+                                        System.out.println("Quarto adicionado com sucesso.");
+                                    }
                                     case 4 -> {
                                         System.out.print("""
-                                        Gerir autenticação:
+                                        \nGerir autenticação:
                                         1. Adicionar credenciais
                                         2. Excluir credenciais
                                         """);
                                         int opcaoGerirAutenticacao = 0;
                                         while (opcaoGerirAutenticacao < 1 || opcaoGerirAutenticacao > 2) {
-                                            System.out.println("Opção (1-2):");
+                                            System.out.print("Opção (1-2): ");
                                             opcaoGerirAutenticacao = input.nextInt();
 
                                             switch (opcaoGerirAutenticacao) {
@@ -182,14 +317,14 @@ public class ex3 {
                         }
                         case 2 -> {
                             System.out.print("""
-                            Consultar clientes:
+                            \nConsultar clientes:
                             1. Por ID
                             2. Por contacto telefonico
                             3. Por email
                             """);
                             int opcaoConsultarClientes = 0;
                             while (opcaoConsultarClientes < 1 || opcaoConsultarClientes > 3) {
-                                System.out.println("Opção (1-3):");
+                                System.out.print("Opção (1-3): ");
                                 opcaoConsultarClientes = input.nextInt();
 
                                 switch (opcaoConsultarClientes) {
@@ -202,14 +337,14 @@ public class ex3 {
                         }
                         case 3 -> {
                             System.out.print("""
-                            Consultar quartos disponíveis:
+                            \nConsultar quartos disponíveis:
                             1. Todos
                             2. Por tema
                             3. Por tipo
                             """);
                             int opcaoConsultarQuartosDisponiveis = 0;
                             while (opcaoConsultarQuartosDisponiveis < 1 || opcaoConsultarQuartosDisponiveis > 3) {
-                                System.out.println("Opção (1-3):");
+                                System.out.print("Opção (1-3): ");
                                 opcaoConsultarQuartosDisponiveis = input.nextInt();
 
                                 switch (opcaoConsultarQuartosDisponiveis) {
@@ -222,13 +357,13 @@ public class ex3 {
                         }
                         case 4 -> {
                             System.out.print("""
-                            Consultar Reservas:
+                            \nConsultar Reservas:
                             1. Histórico de reservas
                             2. Reservas ativas
                             """);
                             int opcaoConsultarReservas = 0;
                             while (opcaoConsultarReservas < 1 || opcaoConsultarReservas > 2) {
-                                System.out.println("Opção (1-2):");
+                                System.out.print("Opção (1-2): ");
                                 opcaoConsultarReservas = input.nextInt();
 
                                 switch (opcaoConsultarReservas) {
@@ -246,8 +381,8 @@ public class ex3 {
                     }
                 }
             }
-//        } catch (Exception e) {
-//            System.out.println(e.getMessage());
-//        }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
